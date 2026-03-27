c633e053c98f274e08e8f02ce611bfb197e35532
Xavier Joseph <xavierjosephh@gmail.com>
2026-03-27 23:13:11 +0530

    Create SlidingWindowMaximumBruteForce.java

---
+++ SlidingWindowMaximumBruteForce.java
@@ -0,0 +1,19 @@
+class SlidingWindowMaximumBruteForce {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int[] result = new int[nums.length -k + 1];
+        for (int i=0; i < nums.length && nums.length -k + 1 > i; i++) {
+            int m = 0;
+            int max = nums[i];
+            for (int j = i; j< nums.length && m < k; j++) {
+                if (nums[j] > max) {
+                    max = nums[j];
+                }   
+                result[i] = max;
+                m ++;
+                System.out.print(nums[j]);
+            }
+            System.out.println("");
+        }
+        return result;
+    }
+}
